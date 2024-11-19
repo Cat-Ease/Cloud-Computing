e84a339e7af3be9bb97999130c45a05cabbf552f
Faizal <155442217+X-Treme123@users.noreply.github.com>
2024-11-19 11:19:43 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,21 +3,21 @@ const { Storage } = require("@google-cloud/storage");
 const path = require("path");
 
 // Tambahkan log untuk memastikan path kredensial
-const keyFilePath = path.join(__dirname, "../config/project-article-442009-e284d6e084c7.json");
+const keyFilePath = path.join(__dirname, "your-credential.json");
 console.log("Key File Path:", keyFilePath);
 
 const db = new Firestore({
   keyFilename: keyFilePath,
-  projectId: "project-article-442009",
+  projectId: "your-project-id",
 });
 
 const storage = new Storage({
   keyFilename: keyFilePath,
-  projectId: "project-article-442009",
+  projectId: "your-project-id",
 });
 
 // Inisialisasi bucket
-const bucket = storage.bucket("my-article-images"); // Pastikan nama bucket sesuai
+const bucket = storage.bucket("your-bucket"); // Pastikan nama bucket sesuai
 console.log("Storage Bucket:", bucket.name);
 
 module.exports = { db, storage, bucket };
